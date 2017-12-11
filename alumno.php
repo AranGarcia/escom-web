8fb9588328f478345306ffc6eeadfcaf8acd8d5c
Aarón Yair <aaronyair16@gmail.com>
2017-12-11 13:28:10 -0600

    Corrección de diseño a alumno.php

--- alumno.php
+++ alumno.php
@@ -29,8 +29,8 @@ $logout_ref = $configs["urls"]["logout"];
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../css/master.css">
-    <link rel="stylesheet" type="text/css" href="../css/alumno.css">
+    <link rel="stylesheet" type="text/css" href="css/master.css">
+    <link rel="stylesheet" type="text/css" href="css/alumno.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400&amp;subset=latin-ext" rel="stylesheet"> 
     <script src="js/jquery-3.2.1.js"></script>
     <title>Inicio</title>
@@ -38,12 +38,10 @@ $logout_ref = $configs["urls"]["logout"];
 <body>
     <div class="contenedor-menu">
         <ul>
-            <?php
             <li>
-                <img class="logo-escom-menu" src="../img/logo_escom_blanco.svg">
-                Bienvenido, $user_name
+                <img class="logo-escom-menu" src="img/logo_escom_blanco.svg">
+                Bienvenido, <?php $user_name?>
             </li>
-            ?>
             <li><a id="tab-invitacion">Invitaci&oacute;n</a></li>
             <li><a id="tab-invitacion">Anuario</a></li>
             <li><a id="tab-invitacion">Perfil</a></li>
