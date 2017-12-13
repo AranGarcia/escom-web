df8575630ccb5389e536852e4a00b866b25cf999
Aarón Yair <aaronyair16@gmail.com>
2017-12-13 06:29:08 -0600

    Incorporación de PDF. Falta que sea dinamico y la carpeta mPDF (Pesa 100MB)

---
+++ php/invitacion-excelencia-pdf.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/pdf-invitacion.css">
+</head>
+<body>
+	<h3>Escuela Superior De C&oacute;mputo</h3>
+	<h1>Entrega de Diplomas a Alumnos de Excelencia</h1>
+	<h2>Ing. en Sistemas Computacionales</h2>
+	<h2>2013 - 2017</h2>
+	<p>Se Otorga la Intitaci&oacute;n A:</p>
+	<h2>Allan de Jesus Garci Alvarez</h2> <!--SALE DE LA BASE-->
+	<h4>8 de Diciembre de 2017</h4> <!--SALE DE LA BASE-->
+	<h4>17:30</h4> <!--SALE DE LA BASE-->
+</body>
+</html>
