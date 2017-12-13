2c6936da4016a5bf5614e28bf2b93019837a2f60
Aran Garcia <doctorarang@gmail.com>
2017-12-12 22:01:11 -0600

    Invitacion de acuerdo a tipo de ceremonia

--- alumno.php
+++ alumno.php
@@ -45,18 +45,18 @@ $ceremonias = obtenerCeremonias($_SESSION["usuario_activo"]);
         <ul>
             <li>
                 <img class="logo-escom-menu" src="img/logo_escom_blanco.svg">
-                Bienvenido, <?php $user_name?>
+                Bienvenido, <?php echo $user_name ?>
             </li>
             <li><a id="tab-invitacion">Invitaci&oacute;n</a></li>
             <!-- Si está invitado a la ceremonia de generacion puede ver el anuario -->
             <?php
-            if(in_array("generacion", $ceremonias)){
+            if (in_array("generacion", $ceremonias)) {
                 echo "<li><a id=\"tab-anuario\">Anuario</a></li>";
             }
             ?>
             <li><a id="tab-perfil">Perfil</a></li>
         </ul>
-        <a href="<?php echo $logout_ref?>" class="op-cerrar-sesion">Cerrar sesi&oacute;n</a>
+        <a href="<?php echo $logout_ref ?>" class="op-cerrar-sesion">Cerrar sesi&oacute;n</a>
     </div>
 
     <!-- __________SECCIONES QUE CAMBIARAN SEGUN LA SELECCION DEL MENU__________ -->
@@ -67,34 +67,42 @@ $ceremonias = obtenerCeremonias($_SESSION["usuario_activo"]);
             <p>Has sido invitado a</p>
 
             <!--Todo esto es el cuadro que muestra la información y debería generarse desde aquí-->
-            <div class="contenedor-tarjeta-inv">
-                <div class="contenedor-mitad">
-                    <h1>ENTREGA DE DIPLOMAS GENERACIÓN 2013 - 2017</h1>
-                    <p>Fecha: 08/Diciembre/2017</p>
-                    <p>Hora: 17:30</p>
-                    <p>Lugar: Auditorio A “Ing. Alejo Peralta” del Centro Cultural Jaime Torres Bodet</p>
-                    <button>Descargar invitaci&oacute;n</button>
-                </div>
-                <!--Este es el mapa de la ubicación del lugar donde se organiza cada evento-->
-                <div class="contenedor-mitad">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.0585465358336!2d-99.13741708509231!3d19.49611678684827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f9b09a806093%3A0x934f6b997bafe6b1!2sAuditorio+Ing.+Alejo+Peralta!5e0!3m2!1ses-419!2smx!4v1513064253103" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-                </div>
-            </div>
+            <?php 
+            if (in_array("generacion", $ceremonias)) {
+                echo "<div class=\"contenedor-tarjeta-inv\">
+                    <div class=\"contenedor-mitad\">
+                        <h1>ENTREGA DE DIPLOMAS GENERACIÓN 2013 - 2017</h1>
+                        <p>Fecha: 08/Diciembre/2017</p>
+                        <p>Hora: 17:30</p>
+                        <p>Lugar: Auditorio A “Ing. Alejo Peralta” del Centro Cultural Jaime Torres Bodet</p>
+                        <button>Descargar invitaci&oacute;n</button>
+                    </div>
+                    <!--Este es el mapa de la ubicación del lugar donde se organiza cada evento-->
+                    <div class=\"contenedor-mitad\">
+                        <iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.0585465358336!2d-99.13741708509231!3d19.49611678684827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f9b09a806093%3A0x934f6b997bafe6b1!2sAuditorio+Ing.+Alejo+Peralta!5e0!3m2!1ses-419!2smx!4v1513064253103\" width=\"100%\" height=\"100%\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>
+                    </div>
+                </div>";
+            }
+            ?>
 
             <!--En caso de tener las dos invitaciones se generarían ambos, aunque tengo duda sobre la información del segundo-->
-            <div class="contenedor-tarjeta-inv">
-                <div class="contenedor-mitad">
-                    <h1>ALUMNOS DE EXCELENCIA ACADÉMICA</h1>
-                    <p>Fecha: 08/Diciembre/2017</p>
-                    <p>Hora: 17:30</p>
-                    <p>Lugar: Auditorio A “Ing. Alejo Peralta” del Centro Cultural Jaime Torres Bodet</p>
-                    <button>Descargar invitaci&oacute;n</button>
-                </div>
-                <!--Este es el mapa de la ubicación del lugar donde se organiza cada evento-->
-                <div class="contenedor-mitad">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.0585465358336!2d-99.13741708509231!3d19.49611678684827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f9b09a806093%3A0x934f6b997bafe6b1!2sAuditorio+Ing.+Alejo+Peralta!5e0!3m2!1ses-419!2smx!4v1513064253103" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-                </div>
-            </div>
+            <?php
+            if (in_array("excelencia", $ceremonias)) {
+                echo "<div class=\"contenedor-tarjeta-inv\">
+                    <div class=\"contenedor-mitad\">
+                        <h1>ALUMNOS DE EXCELENCIA ACADÉMICA</h1>
+                        <p>Fecha: 08/Diciembre/2017</p>
+                        <p>Hora: 17:30</p>
+                        <p>Lugar: Auditorio A “Ing. Alejo Peralta” del Centro Cultural Jaime Torres Bodet</p>
+                        <button>Descargar invitaci&oacute;n</button>
+                    </div>
+                    <!--Este es el mapa de la ubicación del lugar donde se organiza cada evento-->
+                    <div class=\"contenedor-mitad\">
+                        <iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3761.0585465358336!2d-99.13741708509231!3d19.49611678684827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f9b09a806093%3A0x934f6b997bafe6b1!2sAuditorio+Ing.+Alejo+Peralta!5e0!3m2!1ses-419!2smx!4v1513064253103\" width=\"100%\" height=\"100%\" frameborder=\"0\" style=\"border:0\" allowfullscreen></iframe>
+                    </div>
+                </div>";
+            }
+            ?>
         </div>
 
         <!--Aquí está todo lo referente al anuario-->
@@ -129,6 +137,6 @@ $ceremonias = obtenerCeremonias($_SESSION["usuario_activo"]);
         echo var_dump($_SESSION);
         echo "\n-------------------\n";
         echo var_dump($ceremonias);
-    ?></p>
+        ?></p>
 </body>
 </html>
