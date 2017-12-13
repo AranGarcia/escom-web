5b7938c153ae59f032cf4dffcd69866cabdd0426
Aarón Yair <aaronyair16@gmail.com>
2017-12-13 12:26:28 -0600

    Cambios en los PDF

--- php/invitacion-excelencia-pdf.php
+++ php/invitacion-excelencia-pdf.php
@@ -1,3 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include("utils.php");
+$configs = include("config.php");
+session_start();
+
+// Si ningún usuario está activo se regresa a pagina principal
+if (!isset($_SESSION["usuario_activo"])) {
+	header("Location: " . $configs["urls"]["base"]);
+	exit();
+}
+
+// Inicialización de variables
+$user_name = $_SESSION["usuario_nombre"];
+$user_ap = $_SESSION["usuario_ap_pat"];
+$user_am = $_SESSION["usuario_ap_mat"];
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,7 +30,7 @@
 	<h2>Ing. en Sistemas Computacionales</h2>
 	<h2>2013 - 2017</h2>
 	<p>Se Otorga la Intitaci&oacute;n A:</p>
-	<h2>Allan de Jesus Garci Alvarez</h2> <!--SALE DE LA BASE-->
+	<h2><?php echo $user_name?> <?php echo $user_ap?> <?php echo $user_am?></h2> <!--SALE DE LA BASE-->
 	<h4>8 de Diciembre de 2017</h4> <!--SALE DE LA BASE-->
 	<h4>17:30</h4> <!--SALE DE LA BASE-->
 </body>
