9eed3ebda12c20e919aae1410b73e54a3be0c4c5
Aran Garcia <doctorarang@gmail.com>
2017-12-13 03:25:47 -0600

    Funcion: Cargar info alumnos a base de datos desde hoja de calculos

--- php/utils.php
+++ php/utils.php
@@ -13,8 +13,37 @@ function leerHojaDeCalculo()
 
     $excel = new PhpExcelReader;
 
-    $excel->read($configs["path"]["docs"] . "alumnos.xls");
+    // Extraccion de informacion de hoja de calculo
+    $excel->read($configs["path"]["docs"] . "alumnos_generacion.xls");
     $alumnos = array_slice($excel->sheets[0]["cells"], 1);
+
+    //Si no existen renglones con informacion, no hay alumno que insertar
+    if (count($alumnos) == 0) {
+        return;
+    }
+
+    $dbconn = connectDB();
+    $query_insert = "INSERT INTO usuario VALUES ('";
+
+    foreach ($alumnos as $alumno) {
+        $boleta = $alumno[1];
+        $nombre = explode(" ", $alumno[2]);
+        $ultimo = count($nombre) - 1;
+        $nombres = array_slice($nombre, 0, $ultimo - 1);
+        $nom = implode(" ", $nombres);
+        $ap = $nombre[$ultimo - 1];
+        $am = $nombre[$ultimo];
+        $email = $alumno[21];
+
+        $resultado = pg_query($query_insert . $boleta . "','" . $nom . "','" . $ap . "','" . $am .
+            "','password', 'alumno','" . $email . "',true);");
+
+        if (!$resultado) {
+            echo "No se pudo insertar " . $boleta . "<br>";
+        } else {
+            echo "<br>OK<br>";
+        }
+    }
 }
 
 /* 
@@ -24,15 +53,7 @@ Accede al origen de datos para obtener información del usuario.
  */
 function obtenerInfoUsuario($clave)
 {
-    $configs = include("config.php");
-
-    $dbhost = $configs["db"]["host"];
-    $dbname = $configs["db"]["name"];
-    $dbuser = $configs["db"]["user"];
-    $dbpassw = $configs["db"]["passw"];
-
-    $dbconn = pg_connect("host=" . $dbhost . " dbname=" . $dbname . " user=" . $dbuser . " password=" . $dbpassw)
-        or die("Could not connect" . pg_last_error());
+    $dbconn = connectDB();
 
     $query = "SELECT nom_usuario, nom, email, contrasena, rol_usuario, activo, num_intentos 
         FROM usuario WHERE nom_usuario = '" . $clave . "' AND rol_usuario = 'alumno';";
@@ -85,5 +106,18 @@ function unsetAlumnoSession()
     unset($_SESSION["usuario_tipo"]);
     unset($_SESSION["usuario_nombre"]);
     unset($_SESSION["usuario_email"]);
+}
+
+function connectDB()
+{
+    $configs = include("config.php");
+
+    $dbhost = $configs["db"]["host"];
+    $dbname = $configs["db"]["name"];
+    $dbuser = $configs["db"]["user"];
+    $dbpassw = $configs["db"]["passw"];
+
+    return pg_connect("host=" . $dbhost . " dbname=" . $dbname . " user=" . $dbuser . " password=" . $dbpassw)
+        or die("Could not connect" . pg_last_error());
 }
 ?>
